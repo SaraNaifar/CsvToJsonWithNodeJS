a6556def1782be88779ac7e3249f5bc18c3477d3
sara <naifarsara@gmail.com>
2017-12-06 16:03:57 +0100

    stringfiying final result and push it into an array

--- customcsvtojson.js
+++ customcsvtojson.js
@@ -2,19 +2,20 @@ const csv = require('csvtojson')
 const fs= require('fs')
 const csvFilePath = './customer-data.csv'
 const path = require('path')
-var data =''
+var data =[]
 
 csv()
 .fromFile(csvFilePath)
 .on('json',(csvRow)=>{
     //header=> [header1, header2, header3]
-    data +=JSON.stringify(csvRow)+'\n'
+    let chunkOfData=csvRow
+    data.push(chunkOfData)
 
 })
 
 .on('done',(error)=>{
 
-    fs.writeFile('./customer-data.json', data, function (err) {
+    fs.writeFile('./customer-data.json', JSON.stringify(data), function (err) {
         if (err)
             return console.log(err);
         console.log('just check the file');
